57be16fe48a286423ca456f852c1a13b54c35ef0
Justin Kuhn <justintkuhn@gmail.com>
2017-09-22 13:13:33 -0400

    Add files via upload

---
+++ libraries/email.js
@@ -0,0 +1,13 @@
+var email;
+var x;
+var y;
+
+function setup() {
+ createCanvas(1000,1000);
+ noStroke();
+}
+
+function draw() {
+  fill(200);
+  ellipse(100,100,100,100);
+}
---
+++ libraries/sketch.js
@@ -0,0 +1,14 @@
+
+
+var width = 1024;
+var height = 768;
+
+
+function setup() {
+    createCanvas(width, height);
+}
+
+function draw() {
+    rect(100, 100, 100, 100);
+
+}
